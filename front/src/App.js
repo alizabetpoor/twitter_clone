4a66a120dc0f706af4cdd1953f8eef64804cc435
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-09-04 10:58:23 +0430

    update BrowserRouter stage

--- front/src/App.js
+++ front/src/App.js
@@ -1,20 +1,18 @@
 import "./App.css";
 import Layout from "./Layout/Layout";
 import Routes from "./Routes";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Layout>
-          <Switch>
-            {Routes.map((route, index) => {
-              return <Route {...route} key={index} />;
-            })}
-          </Switch>
-        </Layout>
-      </BrowserRouter>
+      <Layout>
+        <Switch>
+          {Routes.map((route, index) => {
+            return <Route {...route} key={index} />;
+          })}
+        </Switch>
+      </Layout>
     </div>
   );
 }
