aa1b404a16ee06c7d91793715930fd3bd7c85bfa
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-09-10 12:50:36 +0430

    update tweet models

--- backend/tweet/models.py
+++ backend/tweet/models.py
@@ -3,11 +3,11 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
-class Tweet(models):
+class Tweet(models.Model):
     author=models.ForeignKey(User,on_delete=models.CASCADE,related_name="tweets")
     text=models.TextField(max_length=350)
-    replies=models.ManyToManyField("self",on_delete=models.SET_NULL,related_name="parent",null=True,default=None,blank=True)
+    replies=models.ManyToManyField("self",related_name="parent",default=None,blank=True)
     created=models.DateTimeField(auto_now_add=True)
     updated=models.DateTimeField(auto_now=True)
-    likes=models.ManyToManyField(User,related_name="liked",default=None,blank=True,null=True)
+    likes=models.ManyToManyField(User,related_name="liked",default=None,blank=True)
 
